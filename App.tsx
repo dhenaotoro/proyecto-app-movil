387fa9352037b48f69723beb01600f9c2ae29fa8
IvanDSanchezC <id.sanchez2@uniandes.edu.co>
2024-10-09 21:06:35 -0500

    Historia de usuario movil HU007M registrar PQRs

--- App.tsx
+++ App.tsx
@@ -1,3 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
 import React from 'react';
 import {
   StyleSheet,
@@ -18,24 +25,5 @@ function App(): React.JSX.Element {
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
