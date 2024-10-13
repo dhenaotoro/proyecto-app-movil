38704a68a48ab39313b013d80076b89de7cfba24
IvanDSanchezC <id.sanchez2@uniandes.edu.co>
2024-10-13 13:24:33 -0500

    Se corrigen las pruebas

--- e2e/Login.e2e.ts
+++ e2e/Login.e2e.ts
@@ -1,5 +1,5 @@
 import { isScreenShown } from "./utils/isScreenShown";
-import { by, element, expect } from 'detox';
+import {by, element, expect} from 'detox';
 import { USER_LOGIN } from "./utils/mockedData";
 
 describe('Login Component', () => {
@@ -14,20 +14,13 @@ describe('Login Component', () => {
   it('should filter properties based on input (positive case)', async () => {
     await isScreenShown('Login');
 
-    // Login process
     await element(by.id('Login.Correo')).typeText(USER_LOGIN.email);
     await element(by.id('Login.Password')).typeText(USER_LOGIN.password);
+
     await element(by.id('Login.Button')).tap();
 
-    // Verify navigation to ListarPQRs
     await isScreenShown('ListarPQRs');
+    
     await expect(element(by.id('ListarPQRs.MainTitle'))).toBeVisible(100);
-
-    // Press the "Registra tu PQR" button
-    await element(by.text('Registra tu PQR')).tap();
-
-    // Verify navigation to CrearPQRs
-    await isScreenShown('CrearPQRs');
-    await expect(element(by.id('CrearPQRs'))).toBeVisible(100);
   });
 });
