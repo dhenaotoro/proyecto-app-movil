1daf9d462b0e19594b0bd69d901635edd1b8f52a
Daniel Felipe Henao Toro <daniel.henao@geniussports.com>
2024-10-30 00:04:44 -0500

    Se agrega Chatbot y algunos test unitarios a la aplicacion movil

--- src/test/components/Header/MainHeader.test.tsx
+++ src/test/components/Header/MainHeader.test.tsx
@@ -10,33 +10,51 @@ jest.mock('../../../context/MenuModalContext');
 describe('MainHeader', () => {
     // Mockea la función openMenu
     const mockOpenMenu = jest.fn();
+    const mockBackPress = jest.fn();
 
     beforeEach(() => {
         // Simula el contexto y su función
         (useMenuModal as jest.Mock).mockReturnValue({ openMenu: mockOpenMenu });
     });
 
+    const renderComponent = () => render(<MainHeader showBackButton showMenu onBackPress={mockBackPress} />);
+
     afterEach(() => {
         jest.clearAllMocks(); // Limpia los mocks después de cada prueba
     });
 
     it('should have a title', () => {
-        render(<MainHeader />);
+        renderComponent();
     
         expect(screen.getByRole('text', { name: 'ABCall'})).toBeTruthy();
     });
 
     it('should have a menu button', () => {
-        render(<MainHeader />);
+        renderComponent();
     
         expect(screen.getByTestId('MainHeader.Button')).toBeTruthy();
     });
 
     it('should call openMenu method from the MenuModalContext', async () => {
         const user = userEvent.setup();
-        render(<MainHeader />);
+        renderComponent();
     
         await user.press(screen.getByTestId('MainHeader.Button'));
         expect(mockOpenMenu).toHaveBeenCalled();
+    });
+
+    it('should have a close button', () => {
+        renderComponent();
+    
+        expect(screen.getByTestId('MainHeader.CloseButton')).toBeTruthy();
+    });
+
+    it('should call closeScreen method', async () => {
+        const user = userEvent.setup();
+        renderComponent();
+    
+        await user.press(screen.getByTestId('MainHeader.CloseButton'));
+
+        expect(mockBackPress).toHaveBeenCalled();
     });
 });
